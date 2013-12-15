b4aea1c25236b0bb01f7c55239b5666c68f9ebf9
Dominik Schmid <DominikSchmid93@gmail.com>
2013-12-15 14:33:51 +1300

    Added increment/reset server

---
+++ server.js
@@ -0,0 +1,37 @@
+var app = require('http').createServer(handler);
+var io = require('socket.io').listen(app);
+var fs = require('fs');
+
+app.listen(8080);
+// app.listen('0.0.0.0:8080');
+
+var count = 0;
+
+function handler (req, res) {
+  fs.readFile(__dirname + '/index.html',
+  function (err, data) {
+    if (err) {
+      res.writeHead(500);
+      return res.end('Error loading index.html');
+    }
+
+    res.writeHead(200);
+    res.end(data);
+  });
+}
+
+io.sockets.on('connection', function (socket) {
+  io.sockets.emit('count', {count: count});
+  socket.on('increment', function (data) {
+    count += 1;
+    io.sockets.emit('count', {count: count});
+    console.log('increment: ' + count);
+  });
+  socket.on('reset', function (data) {
+    count = 0;
+    io.sockets.emit('count', {count: count});
+    console.log('reset: ' + count);
+  });
+});
+
+console.log('running')
